eb781f781917adeecb8db7aba9f3e73595465a06
psmurashov <50335931+psmurashov@users.noreply.github.com>
2024-10-06 10:11:33 +0000

    Update addon.js

--- plugins/addon.js
+++ plugins/addon.js
@@ -3,7 +3,7 @@
 	
 	var plugins = Lampa.Storage.get('plugins','[]')
 	plugins.forEach(function(plug) {
-		plug.url = (plug.url + '').replace('https://scabrum.github.io/plugins/addon.js', 'https://bylampa.github.io/addon.js').replace('http://scabrum.github.io/plugins/addon.js', 'https://bylampa.github.io/addon.js');
+		plug.url = (plug.url + '').replace('https://psmurashov.github.io/bata/plugins/addon.js', 'https://bylampa.github.io/addon.js').replace('https://psmurashov.github.io/bata/plugins/addon.js', 'https://bylampa.github.io/addon.js');
 	})	
 	Lampa.Storage.set('plugins',plugins)
 	
