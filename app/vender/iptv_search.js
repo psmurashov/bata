5c782230ba3ca1c8da79ee898c39f88b5990d693
psmurashov <50335931+psmurashov@users.noreply.github.com>
2024-10-29 20:18:40 +0300

    Add files via upload

--- app/vender/iptv_search.js
+++ app/vender/iptv_search.js
@@ -1,3 +1,4 @@
+/*
 async function iptv_search_name(channelName) {
     try {
         // Загружаем JSON файл
@@ -13,8 +14,37 @@ async function iptv_search_name(channelName) {
         if (channel) {
             return channel.count;
         } else {
-            throw new Error('Channel not found');
+            throw new Error('Channel not found:'+channelName);
         }
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
+}
+*/
+async function iptv_search_name(channelName) {
+    try {
+        // Загружаем JSON файл
+        const response = await fetch('chanal_name.json');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        // Разрешаем промис и получаем данные
+        const channels = await response.json();
+
+        // Проверяем, что channels является массивом
+        if (!Array.isArray(channels)) {
+            throw new Error('Channels data is not an array');
+        }
+
+        for (const channel of channels) {
+            if (channel.name === channelName) {
+                return channel.count;
+            }
+        }
+
+        return null; // Если канал не найден
     } catch (error) {
         console.error('Error:', error);
         return null;
