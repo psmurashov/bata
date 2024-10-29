3dae7d931e8f601ddbfe61822b05a54f07766b63
psmurashov <50335931+psmurashov@users.noreply.github.com>
2024-10-29 18:09:45 +0300

    Add files via upload

--- app/vender/iptv_search.js
+++ app/vender/iptv_search.js
@@ -1,23 +1,47 @@
 async function iptv_search_name(channelName) {
-    const url = 'https://psmurashov.github.io/bata/app/vender/chanal_name.json';
-
     try {
-        const response = await fetch(url);
+        // Загружаем JSON файл
+        const response = await fetch('chanal_name.json');
         if (!response.ok) {
-            throw new Error('Ошибка при загрузке данных: ' + response.statusText);
+            throw new Error('Network response was not ok');
         }
-
         const channels = await response.json();
 
-        for (const channel of channels) {
-            if (channel.name === channelName) {
-                return channel.count;
-            }
-        }
+        // Ищем канал по имени
+        const channel = channels.find(channel => channel.name === channelName);
 
-        return null; // Если канал не найден
+        if (channel) {
+            return channel.count;
+        } else {
+            throw new Error('Channel not found');
+        }
     } catch (error) {
-        console.error('Ошибка при загрузке данных:', error);
+        console.error('Error:', error);
         return null;
     }
 }
+
+/*
+const fs = require('fs');
+const path = require('path');
+
+function iptv_search_name(channelName) {
+    const filePath = path.join(__dirname, 'chanal_name.json');
+    const data = fs.readFileSync(filePath, 'utf8');
+    const channels = JSON.parse(data);
+
+    for (const channel of channels) {
+        if (channel.name === channelName) {
+            return channel.count;
+        }
+    }
+
+    return null; // Если канал не найден
+}
+
+// Пример использования функции
+
+const channelName = "Prokop TV Масяня";
+const count = iptv_search_name(channelName);
+console.log(count); // Выведет 7904, если канал найден
+***/
