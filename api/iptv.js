5b7664367d1aac71af5fd0e6e5a997e168f83ff9
psmurashov <50335931+psmurashov@users.noreply.github.com>
2024-10-30 21:08:15 +0300

    Add files via upload

--- api/iptv.js
+++ api/iptv.js
@@ -506,7 +506,7 @@
         if (string.startsWith('#EXTINF:')) {
           var EXTINF = string;
           items[i] = {
-            name: EXTINF.getName()+'3NAME',
+            name: EXTINF.getName()+ iptv_search_name(EXTINF.getName()).then((count) =>{console.log('1111: '+count)}) +'4NAME',
             tvg: {
               //id: EXTINF.getAttribute('tvg-id'),
               id: EXTINF.getAttribute('tvg-id'),
@@ -781,7 +781,7 @@
                   // Пример использования функции
                   /*id: count_id,*/
                   id: item.tvg && item.tvg.id ? item.tvg.id : null,
-                  name: name.replace(/ \((\+\d+)\)/g, ' $1').replace(/\s+(\s|ⓢ|ⓖ|ⓥ|ⓞ|Ⓢ|Ⓖ|Ⓥ|Ⓞ)/g, ' ').trim()+  name  +'444444444',
+                  name: name.replace(/ \((\+\d+)\)/g, ' $1').replace(/\s+(\s|ⓢ|ⓖ|ⓥ|ⓞ|Ⓢ|Ⓖ|Ⓥ|Ⓞ)/g, ' ').trim()+'444444444',
                   logo: item.tvg && item.tvg.logo && item.tvg.logo.indexOf('http') == 0 ? item.tvg.logo : null,
                   group: item.group.title,
                   url: item.url,
