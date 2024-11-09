e0f3b5cf4e5f6bc9debd30bdb8768f9c65b4666c
psmurashov <50335931+psmurashov@users.noreply.github.com>
2024-11-09 13:31:00 +0300

    Add files via upload

--- app/vender/iptv_search.js
+++ app/vender/iptv_search.js
@@ -13,7 +13,7 @@ let channelNameToCountMap = {};
 
 // Функция для загрузки и обработки chanal_name.json
 function loadChannelNameMap() {
-    return fetch('chanal_name.json')
+    return fetch('channel_name.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Не удалось загрузить chanal_name.json');
@@ -30,55 +30,3 @@ function loadChannelNameMap() {
         });
 }
 
-
-
-
-// Вызов загрузки при инициализации
-//loadChannelNameMap();
-
-
-
-
-/*
-async function iptv_search_name(channelName) {
-    try {
-        // Загружаем JSON файл
-        const response = await fetch('chanal_name.json');
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        // Разрешаем промис и получаем данные
-        const channels = await response.json();
-
-        // Проверяем, что channels является массивом
-        if (!Array.isArray(channels)) {
-            throw new Error('Channels data is not an array');
-        }
-
-        for (const channel of channels) {
-            if (channel.name === channelName) {
-                return channel.count;
-            }
-        }
-
-        return null; // Если канал не найден
-    } catch (error) {
-        console.error('Error:', error);
-        return null;
-    }
-}
-*/
-//NEW
-/*
-function fun_iptv_search(channelName_) {
-  iptv_search_name(channelName_).then(count => {
-    if (count !== null) {
-      return count;
-    }
-  });
-};
-*/
-
-
-///iptv_search_name(channelName_).then((count) => { count });
