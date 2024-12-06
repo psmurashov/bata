1e4c62635b21578aa2a8cd4b200dd83076529cd7
psmurashov <50335931+psmurashov@users.noreply.github.com>
2024-12-07 00:58:59 +0300

    Add files via upload

--- ZNAK/js/url.js
+++ ZNAK/js/url.js
@@ -1,5 +1,5 @@
         document.getElementById('proverka').addEventListener('click', function() {
-            window.location.href = 'https://psmurashov.github.io/bata/ZNAK/can_add.html'; // Замените на нужный URL
+            window.location.href = 'https://psmurashov.github.io/bata/ZNAK/scan_add.html'; // Замените на нужный URL
         });
 
         document.getElementById('scan').addEventListener('click', function() {
